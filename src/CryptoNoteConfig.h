48fd936598e757aaaa75c919d2b826b4bd7f3b8f
NTRXX <35600456+NTRXX@users.noreply.github.com>
2018-01-21 02:38:35 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -108,8 +108,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 const std::initializer_list<const char*> SEED_NODES = {
   //"your_seed_ip1.com:8080",
   //"your_seed_ip2.com:8080",
-  "134.255.237.183:77731;
-  "134.255.237.178:77731;
+  "134.255.237.183:77731";
+  "134.255.237.178:77731";
 };
 
 struct CheckpointData {
